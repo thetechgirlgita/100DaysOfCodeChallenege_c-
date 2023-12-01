0b8798e3962b52a88c07e2808aeda4021d33515d
Gita Khanal <66932135+thetechgirlgita@users.noreply.github.com>
2023-11-30 22:15:45 -0500

    Create HelloWorld.cpp
    Day 1 of C++

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+//This line includes the input/output stream library, which is necessary for using std::cout
+#include <iostream> 
+//This line includes the C standard input/output library to print.
+#include <cstdio>
+
+using namespace std; // it allows you to use elements from the std namespace without explicitly specifying it
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
