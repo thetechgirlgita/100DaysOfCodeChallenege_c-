2a48a2123ca8103d687602f159152930dfa88e34
Gita Khanal <66932135+thetechgirlgita@users.noreply.github.com>
2023-12-23 11:18:22 -0500

    Create binaryToOctal.cpp
    Binary To Octal

---
+++ binaryToOctal.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    long long binaryNumber;
+
+    // Input binary number
+    cout << "Enter a binary number: ";
+    cin >> binaryNumber;
+
+    // Convert binary to octal
+    int octalNumber = 0, decimalNumber = 0, i = 0;
+
+    while (binaryNumber != 0) {
+        decimalNumber += (binaryNumber % 10) * pow(2, i);
+        ++i;
+        binaryNumber /= 10;
+    }
+
+    i = 1;
+
+    while (decimalNumber != 0) {
+        octalNumber += (decimalNumber % 8) * i;
+        decimalNumber /= 8;
+        i *= 10;
+    }
+
+    // Output octal number
+    cout << "Octal equivalent: " << octalNumber << endl;
+
+    return 0;
+}
